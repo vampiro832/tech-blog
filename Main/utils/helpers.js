678846f873d73f5e14f3771d92d5ea045f1f893a
Abraham Reto <abe.lincon30@gmail.com>
2023-06-25 23:17:46 -0400

    "added emoji"

--- Main/utils/helpers.js
+++ Main/utils/helpers.js
@@ -1,14 +1,14 @@
 module.exports = {
   get_emoji: () => {
     const randomNum = Math.random();
-    let book = "📗";
+    let emoji = "U+2764";
 
     if (randomNum > 0.7) {
-      book = "📘";
+      emoji = "U+1F44C";
     } else if (randomNum > 0.4) {
-      book = "📙";
+      emoji = "U+1F919";
     }
 
-    return `<span for="img" aria-label="book">${book}</span>`;
+    return `<span for="img" aria-label="book">${emoji}</span>`;
   },
 };
